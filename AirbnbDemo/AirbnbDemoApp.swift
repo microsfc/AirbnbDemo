1d33c3d8728e0db8207d1d662511e95b0dd7a4e1
sean_liu <sean.liu@moxa.com>
2023-11-17 10:32:27 +0800

    Initial Commit

---
+++ AirbnbDemo/AirbnbDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbDemoApp.swift
+//  AirbnbDemo
+//
+//  Created by SeanLiu-Mac on 2023/11/17.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
